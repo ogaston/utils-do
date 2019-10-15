37efdf6dd4a1734f6f901de4daae836947da52db
Omar Yei Gaston Chalas <ogaston@adm.unapec.edu.do>
2019-10-15 18:03:47 -0400

    closed #5, closed #6 adding the err excecution

--- src/Helper/index.js
+++ src/Helper/index.js
@@ -1,58 +1,77 @@
+const UtilError = require("../Error");
+
 /**
  * Private - define a constant property
  * This function is not pure!
- * @param {Class | Object} obj 
- * @param {string} key 
+ * @param {Class | Object} obj
+ * @param {string} key
  * @param {any} value
- * @returns Class | Object 
+ * @returns Class | Object
  */
 const addConstant = (obj, key, value) => {
-    Object.defineProperty(obj, key, {
-        enumerable: false,
-        configurable: false,
-        writable: false,
-        value: value
-    });
-    return obj
-}
+  Object.defineProperty(obj, key, {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: value
+  });
+  return obj;
+};
 
 /**
  * Private - Search in a source by the key no matter
  * if the parameter has or not accents mark
- * 
- * @param {array} source 
- * @param {string} parameter 
+ *
+ * @param {array} source
+ * @param {string} parameter
  */
 const _searchBy = (source = [], parameter) => {
-    return source.find(
-        item => item.normalize("NFD")
+  return source.find(
+    item =>
+      item
+        .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "")
-        .toLowerCase() ==
-        parameter.toLowerCase()
-    )
-}
+        .toLowerCase() == parameter.toLowerCase()
+  );
+};
+
+/**
+ * Private - Return a handle function of the util error. if isStrict will throw the error, otherwise just show in the console.
+ *
+ * @param {boolean} isStrict
+ * @param {UtilError} errObj
+ */
+const _execError = (isStrict, errObj) => {
+  return isStrict
+    ? () => {
+        throw UtilError(errObj);
+      }
+    : () => {
+        console.error(errObj);
+      };
+};
 
 /**
  * Private - validate the argument and if it fail throw an UtilError
- * @param {any} arg 
+ * @param {any} arg
  */
-const _validateArgs = (arg, errMsg, types = []) => {
-    const _errMsg = errMsg || "the value argument is required and must be string"
-    if (!types.length) {
-        if (typeof arg !== "string") {
-            throw UtilError(_errMsg);
-        }
-    } else {
-        if (!(types.filter(t => typeof arg == t).length)) {
-            throw UtilError(_errMsg);
-        }
-    }
-}
+const _validateArgs = (arg, errMsg, types = [], isStrict = false) => {
+  const _errMsg = errMsg || "the value argument is required and must be string";
+  const errObj = UtilError(_errMsg);
 
+  const execErr = _execError(isStrict, errObj);
+
+  if (!types.length) {
+    if (typeof arg !== "string") execErr();
+  } else {
+    if (!types.filter(t => typeof arg == t).length) {
+      execErr();
+    }
+  }
+};
 
 module.exports = {
-    _validateArgs,
-    _searchBy,
-    addConstant
-}
-
+  _validateArgs,
+  _searchBy,
+  addConstant
+};
--- src/Validator/index.js
+++ src/Validator/index.js
@@ -1,83 +1,90 @@
-const { _searchBy, _validateArgs, addConstant } = require("../Helper/index")
-const rncValidation = require("./rnc-validation")
-
+const { _searchBy, _validateArgs, addConstant } = require("../Helper/index");
+const rncValidation = require("./rnc-validation");
 
 class Validator {
-    /**
-     * Validate if the value inserted is a valid dominican id.
-     *
-     * @param {string | number} id
-     * @returns { boolean }
-     */
-    isAnIde(id) {
-        _validateArgs(id,"the value should be string or number", ["string", "number"])
-        const numericId = ("" + id).match(/\d+/gi).join("");
-        return numericId.length === 11;
+  /**
+   * Validate if the value inserted is a valid dominican id.
+   *
+   * @param {string | number} id
+   * @returns { boolean }
+   */
+  isAnIde(id) {
+    _validateArgs(id, "the value should be string or number", [
+      "string",
+      "number"
+    ]);
+    const regexResult = ("" + id).match(/\d+/gi);
+    const numericId = Array.isArray(regexResult) ? numericId.join("") : "";
+    return numericId.length === 11;
+  }
+
+  /**
+   * Return the value formatted as a dominican id.
+   *
+   * @param {string | number} id
+   */
+  formatToIde(id) {
+    const errMsg = "The value should be string or number";
+    _validateArgs(id, errMsg, ["string", "number"]);
+    const regex = /\(?(\d{3})\)?[- ]?(\d{7})[- ]?(\d{1})/g;
+    const result = ("" + id).replace(regex, "$1-$2-$3");
+    return result.length === 13 ? result : undefined;
+  }
+
+  /**
+   * Validate if the value inserted is a valid dominican telephone.
+   *
+   * @param {string | number} tel
+   * @returns { boolean }
+   */
+  isATel(tel) {
+    _validateArgs(tel, "the value should be string or number", [
+      "string",
+      "number"
+    ]);
+    const regexResult = ("" + tel).match(/\d+/gi);
+    const numericTel = Array.isArray(regexResult)
+      ? regexResult.join("").match(/^8[0-4]9\d{3}\d{4}$/)
+      : null;
+    return numericTel !== null;
+  }
+
+  /**
+   * Return the value formatted as a dominican tel number.
+   *
+   * @param {string | number} id
+   */
+  formatToTel(tel) {
+    const errMsg = "the value should be string or number";
+    _validateArgs(tel, errMsg, ["string", "number"]);
+    const regex = /\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})/g;
+    const result = ("" + tel).replace(regex, "($1) $2-$3");
+    return result.length === 14 ? result : undefined;
+  }
+
+  /**
+   * Validate if is a valid RNC
+   *
+   * @param {string | number} input
+   */
+  isRNC(input) {
+    return rncValidation("" + input);
+  }
+
+  /**
+   * Return the value formatted as a dominican RNC.
+   *
+   * @param {string | number} id
+   */
+  formatToRNC(rnc) {
+    const errMsg = "the value should be string or number";
+    _validateArgs(rnc, errMsg, ["string", "number"]);
+    if (this.isRNC(rnc)) {
+      const regex = /\(?(\d{3})\)?[- ]?(\d{5})[- ]?(\d{1})/g;
+      return ("" + rnc).replace(regex, "$1-$2-$3");
     }
-
-    /**
-     * Return the value formatted as a dominican id.
-     *
-     * @param {string | number} id
-     */
-    formatToIde(id) {
-        const errMsg = "Debe ingresar el valor que sera formateado - formatToIde(string | number)";
-        _validateArgs(id, errMsg, ["string", "number"])
-        const regex = /\(?(\d{3})\)?[- ]?(\d{7})[- ]?(\d{1})/g;
-        const result = ("" + id).replace(regex, "$1-$2-$3");
-        return result.length === 13 ? result : undefined;
-    }
-
-
-    /**
-     * Validate if the value inserted is a valid dominican telephone.
-     *
-     * @param {string | number} tel
-     * @returns { boolean }
-     */
-    isATel(tel) {
-        _validateArgs(tel, "the value should be string or number", ["string", "number"])
-        const numericTel = (""+tel).match(/\d+/gi).join("").match(/^8[0-4]9\d{3}\d{4}$/);
-        return numericTel !== null;
-    }
-
-    /**
-     * Return the value formatted as a dominican tel number.
-     *
-     * @param {string | number} id
-     */
-    formatToTel(tel) {
-        const errMsg = "Debe ingresar el valor que sera formateado - formatToIde(string | number)";
-        _validateArgs(tel, errMsg, ["string", "number"])
-        const regex = /\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})/g;
-        const result = ("" + tel).replace(regex, "($1) $2-$3");
-        return result.length === 14 ? result : undefined;
-    }
-
-    /**
-     * Validate if is a valid RNC
-     * 
-     * @param {string | number} input 
-     */
-    isRNC(input) {
-        return rncValidation("" + input);
-    }
-
-
-    /**
-     * Return the value formatted as a dominican RNC.
-     *
-     * @param {string | number} id
-     */
-    formatToRNC(rnc) {
-        const errMsg = "Debe ingresar el valor que sera formateado - formatToRNC(string | number)";
-        _validateArgs(rnc, errMsg, ["string", "number"])
-        if (this.isRNC(rnc)) {
-            const regex = /\(?(\d{3})\)?[- ]?(\d{5})[- ]?(\d{1})/g;
-            return ("" + rnc).replace(regex, "$1-$2-$3");
-        }
-        return undefined;
-    }
+    return undefined;
+  }
 }
 
-module.exports = new Validator;
+module.exports = new Validator();
